89c51fc7e2005d5663674e65d9ae7dc42a4e5c8e
mimo-android <mimo360@gmail.com>
2015-12-15 18:47:38 +0800

    Initial commit

---
+++ app/src/androidTest/java/com/example/mimo_android/tk3cpt/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.mimo_android.tk3cpt;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
